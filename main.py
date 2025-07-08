94ea060a3bfe3affef7cdd579ffed89086c3244d
vgoud911 <vgoud911@gmail.com>
2025-07-09 02:23:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -118,3 +118,5 @@ def webhook():
         send_message(chat_id, "â ï¸ Unknown command.")
 
     return "ok"
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
