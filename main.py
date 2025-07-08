277bdcb6f096aef24bcfa804b6bfbc4146e50812
vgoud911 <vgoud911@gmail.com>
2025-07-09 02:27:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -119,4 +119,4 @@ def webhook():
 
     return "ok"
     if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
