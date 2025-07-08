d65ccc6fd463fdb02a4b8cb3e682d86adfb4b011
vgoud911 <vgoud911@gmail.com>
2025-07-08 23:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def index():
     return "Bot is running"
 
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
